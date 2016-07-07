8bae632e437fe925db2826c36fea753ce8044b7b
Tara Willman <tara.willman@gmail.com>
2016-07-07 07:16:19 -0700

    Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!(
+  email:    Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+users = User.all
+
+
+30.times do
+  Wiki.create!(
+  user:   users.sample,
+  title:  Faker::Lorem.word,
+  body:   Faker::Lorem.paragraph
+  )
+end
+
+admin = User.create!(
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+member = User.create!(
+  email: 'member@example.com',
+  password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
